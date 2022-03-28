3b0a27079dc44fe93ce334c5581d307702555641
phatthidasu <phatthidasu@protonmail.com>
2022-03-28 10:14:05 +0000

    re-config path for SSG

--- next.config.js
+++ next.config.js
@@ -1,8 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: "/odin-recipes",
-  assetPrefix: "/odin-recipes/"
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  let nextConfig;
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    nextConfig = {
+      reactStrictMode: true,
+      env: {
+        PREFIX: ''
+      }
+    };
+  } else {
+    const url = "/odin-recipes";
+    nextConfig = {
+      reactStrictMode: true,
+      basePath: `${url}`,
+      assetPrefix: `${url}/`,
+      env: {
+        PREFIX: `${url}`
+      }
+    };
+  }
+
+  // const nextConfig = {
+  //   reactStrictMode: true,
+  //   basePath: "/odin-recipes",
+  //   assetPrefix: "/odin-recipes/",
+  //   env: {
+  //     prefix: phase === PHASE_DEVELOPMENT_SERVER ? '' : '/odin-recipes'
+  //   }
+  // };
+
+  console.log(phase, nextConfig);
+  return nextConfig;
+}
